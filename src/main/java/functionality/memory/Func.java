00ee6ba744e5a43c898760179662a9af5d4bbd45
Oliver Comprés Poulsen <oli@live.dk>
2020-03-02 15:13:37 +0100

    Added new branch for memory func

--- src/main/java/functionality/memory/Func.java
+++ src/main/java/functionality/memory/Func.java
@@ -1,5 +1,40 @@
 package functionality.memory;
 
-public class Func{
+import java.util.ArrayList;
 
+import data.UserDTO;
+import functionality.IFunc;
+
+public class Func implements IFunc {
+
+    @Override
+    public UserDTO getUser(int userID) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void updateUser(int userID, String Username, String initials, int CPR, String password,
+            ArrayList<Integer> roles) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void createUser(String Username, String initials, int CPR, String password, ArrayList<Integer> roles) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void deleteUser(int userID) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public ArrayList<UserDTO> getUserList() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
